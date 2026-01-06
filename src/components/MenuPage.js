53bb8a6dddc8276b4b1d18fa5c00271c12189a4d
Светлана <autizm6kola@yandex.com>
2026-01-06 04:43:03 +0300

    36

--- src/components/MenuPage.js
+++ src/components/MenuPage.js
@@ -27,7 +27,7 @@ function MenuPage({ allTasks, onSelectRange }) {
     setCorrectWordsCount(correct);
 
     // ДИАПОЗОН — количество заданий в одной кнопке
-    const rangeSize = 34;
+    const rangeSize = 36;
     const newRangesProgress = {};
 
     for (let i = 0; i < allTasks.length; i += rangeSize) {
